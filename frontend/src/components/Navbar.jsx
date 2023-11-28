b30c3934a38a435b795f66d699f678f09a2596f4
EhsaN Karimi <ehsan.karimmi@gmail.com>
2023-11-28 03:45:34 +0330

    Add: navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import logo from "../assets/images/logo.png";
+import userImage from "../assets/images/avatar_icon.png";
+import { NavbarLinks } from "../utils/NavbarLinks";
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
-    return <div>Navbar</div>;
+    const [selectedLink, setSelectedLink] = useState();
+    useEffect(() => {
+        setSelectedLink(window.location.pathname);
+    }, []);
+    return (
+        <nav className="navbar p-3 flex justify-between items-center">
+            {/* logo */}
+            <Link to="/">
+                <img src={logo} alt="Medicare Logo" />
+            </Link>
+            {/* links */}
+            <div>
+                <ul className=" flex justify-start items-center gap-5">
+                    {NavbarLinks.map((link) => {
+                        return (
+                            <NavLink to={link.path} key={link.id}>
+                                <li
+                                    onClick={() => setSelectedLink(link.path)}
+                                    className={`font-semibold transition-all duration-200 ${
+                                        selectedLink === link.path
+                                            ? ` text-primaryColor `
+                                            : ` text-black`
+                                    }`}
+                                >
+                                    {link.title}
+                                </li>
+                            </NavLink>
+                        );
+                    })}
+                </ul>
+            </div>
+            {/* user information */}
+            <div className="flex justify-start items-center gap-5">
+                <figure>
+                    <img src={userImage} alt="User Image" className="rounded-full w-8 h-8" />
+                </figure>
+                <Link to="/login">
+                    <button className="font-medium bg-primaryColor px-3 py-1 rounded-md text-white">
+                        Login
+                    </button>
+                </Link>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
---
+++ frontend/src/utils/NavbarLinks.js
@@ -0,0 +1,22 @@
+export const NavbarLinks = [
+    {
+        id: 1,
+        title: "Home",
+        path: "/",
+    },
+    {
+        id: 2,
+        title: "Find a Doctor",
+        path: "/doctors",
+    },
+    {
+        id: 3,
+        title: "Services",
+        path: "/services",
+    },
+    {
+        id: 4,
+        title: "Contact",
+        path: "/contact",
+    },
+];
